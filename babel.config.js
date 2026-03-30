ca122eb100bb7bf5025e8ee4b51225cda6a0d2e7
Mercy Wanji <kmercyafk@gmail.com>
2026-03-30 14:25:02 +0300

    Fix Turbo JS compilation with modern JS for delete button

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,17 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
 
   const presets = [
-    '@babel/preset-env'
+    ["@babel/preset-env", { targets: "> 0.25%, not dead" }]
   ];
 
   const plugins = [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-private-methods',
-    '@babel/plugin-proposal-private-property-in-object'
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    ["@babel/plugin-proposal-private-methods", { loose: true }],
+    ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
+    "@babel/plugin-proposal-optional-chaining",
+    "@babel/plugin-proposal-logical-assignment-operators",
+    "@babel/plugin-proposal-nullish-coalescing-operator"  // <-- this one fixes ??
   ];
 
   return { presets, plugins };
