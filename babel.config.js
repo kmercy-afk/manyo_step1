f43e8b66d4a64dcc476c89390036bac55f8c97c0
Mercy Wanji <kmercyafk@gmail.com>
2026-03-31 14:10:04 +0300

    add babel.config.js to fix babel-loader getOptions error

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: "> 0.25%, not dead" }]
-  ],
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-private-methods',
-    '@babel/plugin-proposal-private-property-in-object'
+    ['@babel/preset-env', { targets: "defaults" }]
   ]
 };
